88a380ae25295c888857309c6a2b389472c4d7e1
Yordan Yordanov <yoyordanov94@gmail.com>
2025-09-13 19:10:39 +0300

    wip

--- resources/views/components/blobs.blade.php
+++ resources/views/components/blobs.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->class(['absolute inset-0 -z-20 pointer-events-none']) }}>
+<div {{ $attributes->class(['absolute inset-0 -z-20 pointer-events-none overflow-hidden']) }}>
     <div class="relative h-[62.5rem] w-[62.5rem] flex flex-col justify-center items-center -z-20 opacity-15">
         <div class="rounded-full blur-3xl bg-sky-400 absolute top-[25rem] left-[12.5rem] h-[25rem] w-[25rem] animate-[sky_8s_infinite_ease]"></div>
         <div class="rounded-full blur-3xl bg-red-400 absolute right-0 top-[37.5rem] h-[31.25rem] w-[25rem] animate-[red_8s_infinite_linear]"></div>
